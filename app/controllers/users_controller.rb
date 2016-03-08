7e1a90575cef8d3351f64e6d77291a3cb86de235
daisuke_rikiyama <walker.cp.48710@gmail.com>
2016-03-08 03:41:04 +0000

    #19 account activation controller/view create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
-        flash[:info] = "サービスを利用する時はログインしてください。"
-        redirect_to @user, notice: "会員登録が完了しました。"
+        @user.send_activation_email
+        flash[:info] = "メールをチェックしてアカウントを有効化してください。"
+        redirect_to root_url
       else
         render 'new'
       end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -25,6 +25,13 @@ module SessionsHelper
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # ユーザーの永続セッションを忘れる
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
   
   # 現在のユーザーをログアウトする
