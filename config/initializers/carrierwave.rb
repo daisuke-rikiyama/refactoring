0b5943fe49159619d466dd53b380cfcf22d86cdb
daisuke_rikiyama <walker.cp.48710@gmail.com>
2016-03-09 14:04:03 +0000

    #21 heroku S3 upload settings

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => Rails.application.secrets.aws_access_key_id,
+      :aws_secret_access_key  => Rails.application.secrets.aws_secret_key,
+      :region                 => Rails.application.secrets.s3_region
+    }
+    config.fog_directory = Rails.application.secrets.s3_bucket
+  end
+end
