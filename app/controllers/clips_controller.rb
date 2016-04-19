0145ed38c3724585619d0564a2a6898a274b46ab
daisuke_rikiyama <walker.cp.48710@gmail.com>
2016-04-19 02:17:25 +0000

    #23 clip button created

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -2,11 +2,13 @@ class ClipsController < ApplicationController
     before_action :logged_in_user
     
     def create
+        @message_board = MessageBoard.find(params[:message_board_id])
         @message = Message.find(params[:message_id])
         current_user.clip(@message)
     end
     
     def destroy
+        @message_board = MessageBoard.find(params[:message_board_id])
         @message = current_user.clips.find(params[:id]).message
         current_user.unclip(@message)
     end
